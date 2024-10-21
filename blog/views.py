7b9e24a0fa6ea82d4f99b80d0d611eda933e51ab
Briar <supersecretdeer27@gmail.com>
2024-10-20 23:22:36 -0700

    add list and detail views for blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
+from django.views import generic
 from django.shortcuts import render
 
+from .models import Blogger, BlogPost, Comment
+
 # Create your views here.
+def index(request):
+    # Render the HTML template index.html
+    return render(request, 'index.html')
+
+class BlogPostListView(generic.ListView):
+    model = BlogPost
+
+class BlogPostDetailView(generic.DetailView):
+    model = BlogPost
