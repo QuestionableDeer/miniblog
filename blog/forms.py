126c297ff3c5a9092bf069aca51e69d5bc00ea37
Briar <supersecretdeer27@gmail.com>
2024-10-23 14:56:48 -0700

    add form for logged-in users to submit comments on blog posts

---
+++ blog/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class CommentForm(forms.Form):
+    description = forms.CharField(widget=forms.Textarea, help_text='Enter comment about the blog here.')
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,45 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import User
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Blogger, BlogPost, Comment
+from blog.forms import CommentForm
 
 # Create your views here.
 def index(request):
     # Render the HTML template index.html
     return render(request, 'index.html')
+
+@login_required
+def post_comment(request, pk):
+    '''View function for posting a comment to a specific blog'''
+    blog_post = get_object_or_404(BlogPost, pk=pk)
+
+    if request.method == 'POST':
+        # create form instance and populate it with data from request (binding)
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = Comment()
+            comment.author = request.user
+            comment.blog = blog_post
+            comment.description = form.cleaned_data['description']
+
+            comment.save()
+
+            return HttpResponseRedirect(reverse('blog-post-detail', kwargs={'pk':pk}))
+    else:
+        form = CommentForm()
+
+    context = {
+        'form': form,
+        'blog_post': blog_post,
+    }
+
+    return render(request, 'blog/post_comment.html', context)
 
 class BlogPostListView(generic.ListView):
     model = BlogPost
